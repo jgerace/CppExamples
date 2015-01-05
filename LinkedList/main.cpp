6cbb14e270fe2fd83f27b8b748c3f984491308fc
John Gerace <johngerace@gmail.com>
2015-01-05 17:59:49 -0500

    Added LinkedList

---
+++ LinkedList/main.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+       int data;
+       Node *next;
+};
+
+Node *insertFront(Node *head, int data) // to modify head in place, use Node **head
+{
+     Node *n = new Node;
+     n->data = data;
+     n->next = head;
+
+     head = n;
+
+     return head;
+}
+
+bool find(Node *head, int data) // is a piece of data in the list?
+{
+     while(head != NULL) {
+        if(head->data == data) {
+           return true;
+        }
+        else {
+           head = head->next;
+        }
+     }
+
+     return false;
+}
+
+void printList(Node *head)
+{
+     while(head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+     };
+     cout << endl;
+}
+
+void deleteList(Node **head)
+{
+     Node *deleteMe = *head;
+
+     while(deleteMe != NULL) {
+        Node *next = deleteMe->next;
+        delete deleteMe;
+        deleteMe = next;
+     }
+
+     *head = NULL; // prevent dangling pointer
+}
+
+int main(int argc, char *argv[])
+{
+    Node *head = NULL; // initialize just to be safe
+
+    head = insertFront(head, 1);
+    head = insertFront(head, 2);
+
+    printList(head);
+
+    if(find(head, 2)) {
+       cerr << "2 is in list" << endl;
+    }
+
+    if(!find(head, 3)) {
+       cerr << "3 is not in list" << endl;
+    }
+
+    cerr << "deleting" << endl;
+    deleteList(&head);
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
